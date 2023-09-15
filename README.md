22c2f597c15706590a68f3595d59394a0fa1fa19
mtalli <mauno.talli@hotmail.com>
2023-09-15 12:48:38 +0300

    Made a new version of the exercise using regex(regular expressions).

--- README.md
+++ README.md
@@ -1,8 +1,8 @@
-go-reloaded
-
 FOR AUDITORS!
 
-Clone the repo, write your sample in sample.txt and in order to test, write this in your terminal
-go run main.go sample.txt result.txt
+Clone the repo, write your sample in sample.txt and in order to test the program, write this in your terminal
+go run . sample.txt result.txt
 
-This should output result.txt which shows the result.
+This should output result.txt which shows the result of your written sample.
+
+9/15/2023 - Made a new version of the exercise using regex(regular expressions). 
--- main.go
+++ main.go
@@ -2,139 +2,104 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
 	"os"
+	"regexp"
 	"strconv"
 	"strings"
-	"unicode/utf8"
 )
 
-var (
-	wordArray []string
-	flag      bool
-)
-
-func CheckBracket(pos int) int {
-	switch wordArray[pos][:4] {
-	case "(bin":
-		ConvertNumber(pos-1, 2)
-	case "(hex":
-		ConvertNumber(pos-1, 16)
-	case "(cap":
-		ConvertString(pos, strings.Title)
-	case "(up,", "(up)":
-		ConvertString(pos, strings.ToUpper)
-	case "(low":
-		ConvertString(pos, strings.ToLower)
-	default:
-		pos++
+func transformWords(words []string, transformFunc func(string) string) string {
+	for i := range words {
+		words[i] = transformFunc(words[i])
 	}
-	return pos - 1
+	return strings.Join(words, " ")
 }
 
-func ConvertNumber(pos, base int) {
-	decimalValue, err := strconv.ParseInt(wordArray[pos], base, 64)
-	if err != nil {
-		fmt.Println("Error: Unable to convert to decimal.")
-		os.Exit(1)
+func transformAndReplace(match string, submatches []string, transformFunc func(string) string) string {
+	count, _ := strconv.Atoi(submatches[2])
+	if count == 0 {
+		count = 1
 	}
-	wordArray[pos] = strconv.Itoa(int(decimalValue))
-	removeItems(pos+1, 1)
-}
-
-func ConvertString(pos int, transformFunc func(string) string) {
-	if len(wordArray)-1 == pos {
-		wordArray[pos-1] = transformFunc(wordArray[pos-1])
-		removeItems(pos, 1)
-		return
-	}
-
-	count, err := strconv.Atoi(wordArray[pos+1][:len(wordArray[pos+1])-1])
-	if err == nil {
-		for index := pos - 1; index >= pos-int(count); index-- {
-			wordArray[index] = transformFunc(wordArray[index])
-		}
-		removeItems(pos, count)
-	} else {
-		wordArray[pos-1] = transformFunc(wordArray[pos-1])
-		removeItems(pos, 1)
-	}
-}
-
-func CorrectPunctuation(pos int) int {
-	if strings.ContainsRune(".,!?:;", rune(wordArray[pos][0])) {
-		wordArray[pos-1] += string(wordArray[pos][0])
-		wordArray[pos] = wordArray[pos][1:]
-		if len(wordArray[pos]) == 0 {
-			removeItems(pos, 1)
-			pos--
-		}
-		pos = CorrectPunctuation(pos)
-	}
-	return pos
-}
-
-func HandleArticles(pos int) {
-	firstLetter := string(wordArray[pos+1][0])
-	if strings.ContainsAny(firstLetter, "aeiouh") && (wordArray[pos] == "a" || wordArray[pos] == "A") {
-		wordArray[pos] += "n"
-	} else if !strings.ContainsAny(firstLetter, "aeiouh") && (wordArray[pos] == "an" || wordArray[pos] == "An") {
-		wordArray[pos] = string(wordArray[pos][0])
-	}
-}
-
-func AdjustCommas(pos int) {
-	initial, size1 := utf8.DecodeRuneInString(wordArray[pos])
-	final, size2 := utf8.DecodeLastRune([]byte(wordArray[pos]))
-
-	if size1 != 3 || size2 != 3 {
-		return
-	}
-
-	if flag {
-		if string(final) == "‘" {
-			wordArray[pos] = wordArray[pos][size2:] + "’"
-		}
-		if wordArray[pos] == "’" {
-			wordArray[pos-1] += "’"
-			removeItems(pos, 1)
-		}
-		flag = false
-	} else {
-		if string(initial) == "’" {
-			wordArray[pos] = "‘" + wordArray[pos][size1:]
-		}
-		if wordArray[pos] == "‘" {
-			wordArray[pos+1] = "‘" + wordArray[pos+1]
-			removeItems(pos, 1)
-		}
-		flag = true
-	}
-}
-
-func ParseContent() {
-	fileContent, err := os.ReadFile(os.Args[1])
-	if err != nil {
-		fmt.Println("Error: File not found.")
-		os.Exit(1)
-	}
-	wordArray = strings.Fields(string(fileContent))
-}
-
-func removeItems(index, count int) {
-	wordArray = append(wordArray[:index], wordArray[index+count:]...)
+	words := strings.Fields(submatches[1])
+	wordsTransformed := transformWords(words[len(words)-count:], transformFunc)
+	return strings.Join(append(words[:len(words)-count], wordsTransformed), " ")
 }
 
 func main() {
-	ParseContent()
-	for i := 0; i < len(wordArray); i++ {
-		if len(wordArray[i]) > 3 {
-			i = CheckBracket(i)
-		}
-		i = CorrectPunctuation(i)
-		if i != len(wordArray)-1 {
-			HandleArticles(i)
-		}
-		AdjustCommas(i)
+	if len(os.Args) < 3 {
+		fmt.Println("Usage: go run . <input_file> <output_file>")
+		return
 	}
-	os.WriteFile("result.txt", []byte(strings.Join(wordArray, " ")), 0644)
+
+	inputFile := os.Args[1]
+	inputBytes, err := ioutil.ReadFile(inputFile)
+	if err != nil {
+		fmt.Println("Error reading input file:", err)
+		return
+	}
+
+	input := string(inputBytes)
+
+	transformFunctions := []struct {
+		pattern *regexp.Regexp
+		replace func(string) string
+	}{
+		{regexp.MustCompile(`([0-9A-Fa-f]+) \(hex\)`), func(match string) string {
+			hexStr := regexp.MustCompile(`([0-9A-Fa-f]+) \(hex\)`).FindStringSubmatch(match)[1]
+			decimalVal, _ := strconv.ParseInt(hexStr, 16, 64)
+			return strconv.Itoa(int(decimalVal))
+		}},
+		{regexp.MustCompile(`([01]+) \(bin\)`), func(match string) string {
+			binStr := regexp.MustCompile(`([01]+) \(bin\)`).FindStringSubmatch(match)[1]
+			decimalVal, _ := strconv.ParseInt(binStr, 2, 64)
+			return strconv.Itoa(int(decimalVal))
+		}},
+		{regexp.MustCompile(`(\w+(?:\s+\w+)*) \(up(?:, (\d+))?\)`), func(match string) string {
+			submatches := regexp.MustCompile(`(\w+(?:\s+\w+)*) \(up(?:, (\d+))?\)`).FindStringSubmatch(match)
+			return transformAndReplace(match, submatches, strings.ToUpper)
+		}},
+		{regexp.MustCompile(`(\w+(?:\s+\w+)*) \(low(?:, (\d+))?\)`), func(match string) string {
+			submatches := regexp.MustCompile(`(\w+(?:\s+\w+)*) \(low(?:, (\d+))?\)`).FindStringSubmatch(match)
+			return transformAndReplace(match, submatches, strings.ToLower)
+		}},
+		{regexp.MustCompile(`(\w+(?:\s+\w+)*) \(cap(?:, (\d+))?\)`), func(match string) string {
+			submatches := regexp.MustCompile(`(\w+(?:\s+\w+)*) \(cap(?:, (\d+))?\)`).FindStringSubmatch(match)
+			return transformAndReplace(match, submatches, strings.Title)
+		}},
+		{regexp.MustCompile(`'\s*([^']*)\s*'`), func(match string) string {
+			trimmed := strings.TrimSpace(regexp.MustCompile(`'\s*([^']*)\s*'`).FindStringSubmatch(match)[1])
+			return "'" + trimmed + "'"
+		}},
+		{regexp.MustCompile(`\ba\b\s+([aeiouhAEIOUH])`), func(match string) string {
+			return "an " + strings.TrimSpace(match[2:])
+		}},
+	}
+
+	for _, tf := range transformFunctions {
+		input = tf.pattern.ReplaceAllStringFunc(input, tf.replace)
+	}
+
+	// Additional cleanup for punctuation
+	cleanupPatterns := []string{` :`, ` \.`, ` \?`, ` \!`}
+	for _, pattern := range cleanupPatterns {
+		re := regexp.MustCompile(pattern)
+		input = re.ReplaceAllStringFunc(input, func(match string) string {
+			return strings.TrimSpace(match)
+		})
+	}
+
+	// Remove space before comma
+	input = regexp.MustCompile(` ,`).ReplaceAllString(input, ",")
+
+	// Add space after each comma if it's not already there
+	input = regexp.MustCompile(`,([^ ])`).ReplaceAllString(input, ", $1")
+
+	// Write to output file
+	outputFile := os.Args[2]
+	err = ioutil.WriteFile(outputFile, []byte(strings.TrimSpace(input)), 0644)
+	if err != nil {
+		fmt.Println("Error writing output file:", err)
+		return
+	}
 }
